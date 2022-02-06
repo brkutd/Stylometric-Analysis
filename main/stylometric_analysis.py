52176c1b117d0bf1c74ed045ebf002f1d3eec532
Krystian Borkowski <krystianborkowski1998@gmail.com>
2022-02-06 18:51:34 +0100

    update stylometric_analysis.py

--- main/stylometric_analysis.py
+++ main/stylometric_analysis.py
@@ -6,8 +6,29 @@ import matplotlib.pyplot as plt
 LINES = ['-', ':', '--']
 
 
+def text_to_string(filename):
+
+
+
+
 def main():
-    authors_with_strings = dict()
-    authors_with_strings['doyle'] = text_to_string('hound.txt')
-    authors_with_strings['wells'] = text_to_string('war.txt')
-    authors_with_strings['unknown'] = text_to_string('lost.txt')
+    # create a dictionary with authors and their books converted into strings
+    # 'unknown' is the object of study to assign it later to one of the two authors with stylometry
+    strings_by_author = dict()
+    strings_by_author['doyle'] = text_to_string('hound.txt')
+    strings_by_author['wells'] = text_to_string('war.txt')
+    strings_by_author['unknown'] = text_to_string('lost.txt')
+    # test print
+    print(strings_by_author['doyle'][:300])
+    # form a dictionary with authors and their book converted into arrays of words
+    words_by_author = make_word_dict(strings_by_author)
+    # truncation - find the shortest corpus to cut the ends of the other two corpora
+    len_shortest_corpus = find_shortest_corpus(words_by_author)
+
+    word_length_test(words_by_author, len_shortest_corpus)
+    stopwords_test(words_by_author, len_shortest_corpus)
+    parts_of_speech_test(words_by_author, len_shortest_corpus)
+    vocab_test(words_by_author)
+    jaccard_test(words_by_author, len_shortest_corpus)
+
+
