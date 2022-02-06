2f17ee2098c396f58c57982460964a77e9b6c285
Krystian Borkowski <krystianborkowski1998@gmail.com>
2022-02-06 18:17:50 +0100

    update stylometric_analysis.py

--- main/stylometric_analysis.py
+++ main/stylometric_analysis.py
@@ -1,3 +1,13 @@
 import nltk
 from nltk import punkt
 from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+
+LINES = ['-', ':', '--']
+
+
+def main():
+    authors_with_strings = dict()
+    authors_with_strings['doyle'] = text_to_string('hound.txt')
+    authors_with_strings['wells'] = text_to_string('war.txt')
+    authors_with_strings['unknown'] = text_to_string('lost.txt')
